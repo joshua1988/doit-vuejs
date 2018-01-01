9c1408a032a57ebea7de1bec84f414001e99bfb3
joshua1988 <jangkeehyo@gmail.com>
2018-01-01 20:58:49 +0900

    Added the vue-todo application sample

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,67 @@
+<template>
+  <section>
+    <ul v-if="passedData">
+      <transition-group name="list" tag="p">
+        <li v-for="(todoItem, index) in passedData" :key="todoItem" class="shadow">
+          <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+          {{ todoItem }}
+          <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+            <i class="fa fa-trash-o" aria-hidden="true"></i>
+          </span>
+        </li>
+      </transition-group>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['passedData'],
+  methods: {
+    removeTodo(todoItem, index) {
+      this.$emit('removeTodo', todoItem, index);
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+
+/* transition effect */
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active for <2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>

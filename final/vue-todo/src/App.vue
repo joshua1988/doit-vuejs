9147e40e66b418b3d33ae520b029a4948fa4a05b
joshua1988 <jangkeehyo@gmail.com>
2018-01-15 23:26:09 +0900

    Left the comment out

--- final/vue-todo/src/App.vue
+++ final/vue-todo/src/App.vue
@@ -34,7 +34,6 @@ export default {
     }
   },
   created() {
-    // 수정
 		if (localStorage.length > 0) {
 			for (var i = 0; i < localStorage.length; i++) {
 				this.todoItems.push(localStorage.key(i));

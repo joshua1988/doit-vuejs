c3f1bb5198ef9be81718da318aa75a23702b80bf
jhsbeat <jhsbeat@gmail.com>
2018-05-10 01:47:05 +0900

    Fixed a bug firing keyup event twice on vue-todo input.
    MacOSX - chrome 에서 한글입력후 엔터키 입력시 addTodo가 두번 호출되어 경고 modal이 보여지는 버그 수정.

--- final/vue-todo/src/components/TodoInput.vue
+++ final/vue-todo/src/components/TodoInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="inputBox shadow">
-    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keypress.enter="addTodo">
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
